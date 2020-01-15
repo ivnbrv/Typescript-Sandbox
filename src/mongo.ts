db8e4e9a1d55bd040c759223674b15e700ab1cca
Ivan <ivnbrv@icloud.com>
2020-01-14 17:17:09 -0800

    Mongo driver

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,10 +1,35 @@
-import MongoClient from 'mongodb';
+import { Db, MongoClient } from 'mongodb';
+import assert from 'assert';
 
-const url = 'mongodb://127.0.0.1:27017'
-const dbName = 'sandbox'
-const client = MongoClient.connect(url, (error, client) => {
 
-    var db = client.db('test');
-    console.log(db)
-})
+export class Mongo {
+    private client: any;
+    private readonly connection = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017';
+    private readonly database = process.env.DB_NAME || 'test-drive';
+
+    public close() {
+        
+        if (this.client) {
+            this.client.close()
+            .then()
+            .catch((error: any) => {
+                console.error(error);
+            });
+        } else {
+            console.error('close: client is undefined');
+        }
+    }
+
+    public async connect() {
+        try {
+            if (!this.client) {
+                this.client = await MongoClient.connect(this.connection, { 'useNewUrlParser': true, useUnifiedTopology: true });
+            }
+        } catch(error) {
+            console.error(error);
+        }
+    }
+}
+
+const driverd = new Mongo();
 
